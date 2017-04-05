77bf1681f088cedf68b7a94f4ddc8b91d4117036
Unknown <crschriever@gmail.com>
2017-04-04 20:40:44 -0400

    Removing commented out code

--- main.js
+++ main.js
@@ -1,68 +1,69 @@
 var $tTextCont, $tCont, $tNavCont, $slows, 
 	$projectHeadT, $projectHeadCont,
-	$mobileDet, $wrapper, $projectDesc, projectMoveable = true;
+	$mobileDet, $wrapper, $projectDesc, projectMoveable = true,
+	$projects;
 
-/*var projectsArray = {
+var projectsArray = {
 
 	ER: {
 		title: "ER Schedule", 
 		description: 
 			"This is a PHP based web app that is still in development. It will create work schedules for Emergency Rooms, a task that normally takes an employee a full day to do.",
-		outcomes: 
+		/*outcomes: 
 			"From this project I've gained much experience using the OOP side of PHP. I hope to eventually have a working prototype hosted on Digital Ocean within the year.",
 		lessons: 
-			"I've already learned a lot about OOP PHP. I also hope to use this project to learn how to create a server using Digital Ocean."
+			"I've already learned a lot about OOP PHP. I also hope to use this project to learn how to create a server using Digital Ocean."*/
 	},
 	NEAT: {
 		title: "NEAT", 
 		description: 
 			"NEAT stands for Neuro-Evolution of Augmenting Topologies. It is a machine learning algorithm that creates and evolves neural networks to solve a task.",
-		outcomes: 
+		/*outcomes: 
 			"I created a Java library that implemented the NEAT algorithm. I tested the library in various ways and found that my implementation tended to merely memorize the problem rather than gain a deeper understanding for it (if the task was varied even slightly the neural network would struggle). I plan to revisit the library and improve it in the future.",
 		lessons:
-			"From this project I learned the concepts of Machine Learning and Neural Networks."
+			"From this project I learned the concepts of Machine Learning and Neural Networks."*/
 	},
 	dSA: {
 		title: "deSchApps", 
 		description:
 			"A support website for the iPhone Apps i created. The website has a CMS used for easy addition of new Apps.",
-		outcomes:
+		/*outcomes:
 			"A website with details on each of the apps I've worked on. I hope to improve the website's mobile version for I created if before I knew BootStrap.",
 		lessons:
-			"I learned HTML, CSS, JavaScript, PHP, and SQL through this project. I also learned the process for publishing a website through HostGator."
+			"I learned HTML, CSS, JavaScript, PHP, and SQL through this project. I also learned the process for publishing a website through HostGator."*/
 	},
 	HH: {
 		title: "Homework Help", 
 		description:
 			"Homework Help combines scrap paper and a calculator into one app. It provides a means of sorting assignments through the app.",
-		outcomes:
+		/*outcomes:
 			"This was submitted to a Congressional STEM competition and won. It was also on the App Store but has since been removed for monetary reasons.",
 		lessons:
-			"I learned Objective C, how to navigate XCode, and the process of submitting an app to the app store."
+			"I learned Objective C, how to navigate XCode, and the process of submitting an app to the app store."*/
 	},
 	IGW: {
 		title: "Intergalactic Warfare", 
 		description:
 			"Intergalactic Warfare is an iPhone game that consists of firing missiles at your opponents. These missiles must travel through an obstacle course of planets that all have a gravitational pull that affects the path of the missile",
-		outcomes:
+		/*outcomes:
 			"This app was on the app store but has since been removed for monetary reasons.",
 		lessons:
-			"Through the creation of this app I learned how to use XCode's SpriteKit library."
+			"Through the creation of this app I learned how to use XCode's SpriteKit library."*/
 	},
 	DSA: {
 		title: "Darlington School App", 
 		description:
 			"This is an app created for my upper school. It displayed to user's grades, assignments, announcements, schedules, etc.",
-		outcomes:
+		/*outcomes:
 			"I took an independent study at my high school and created this app using PhoneGap and HTML.",
 		lessons:
-			"I learned how to work with PhoneGap and JQuery. My Javascript, HTML, and CSS skills were also vastly improved."
+			"I learned how to work with PhoneGap and JQuery. My Javascript, HTML, and CSS skills were also vastly improved."*/
 	}
 
-};*/
+};
 
-var selectedProject = projectsArray.ER;
-console.log(selectedProject);
+// var selectedProject = projectsArray.ER;
+// console.log(selectedProject);
 
 var mobile = false;
 
@@ -75,23 +76,28 @@ function startupStyling(){
 	$projectHeadCont = $('.project-head-container');
 	$projectHeadT = $('.project-head-text');
 	$wrapper = $('.wrapper');
-	/*$projectDesc = $('.project-description');
-	$projectDesc.title = $projectDesc.find(".title-h");
-	$projectDesc.description = $projectDesc.find(".description-p");
-	$projectDesc.outcomes = $projectDesc.find(".outcomes-p");
-	$projectDesc.lessons = $projectDesc.find(".lessons-p");
-	$projectDesc.heightFinder = $projectDesc.find(".content");*/
 
 	$( window ).resize(onResize);
+
+	//Array of projects
+	$projects = [];
+	$('.project').each(function(){
+		$t = $(this);
+		$projects.push($t);
+		$t.text = $($t.find('p'));
+	});
+
 
 	onResize();
 	scrollSet();
 
+	//Array of scrolling background
 	$slows = [];
-	$('.slow-scroll').each(function(){
+	$('.slow-scroSll').each(function(){
 		$slows.push($(this));
 	});
 
+	//Scrolling background
 	$(document).on('scroll', function(){
 		for (var i = $slows.length - 1; i >= 0; i--) {
 			var slow = $slows[i];
@@ -100,49 +106,25 @@ function startupStyling(){
 		};
 	});
 
-	$('.project').on('click', function(){
-		if(projectMoveable) {
-			projectMoveable = false
-			
-			var id = $(this).attr('id');
-			if(id == "ER") {
-				selectedProject = projectsArray.ER;
-			} else if(id == "NEAT") {
-				selectedProject = projectsArray.NEAT;
-			} else if(id == "dSA") {
-				selectedProject = projectsArray.dSA;
-			} else if(id == "HH") {
-				selectedProject = projectsArray.HH;
-			} else if(id == "IGW") {
-				selectedProject = projectsArray.IGW;
-			} else if(id == "DS") {
-				selectedProject = projectsArray.DSA;
-			}
-
-			/*$projectDesc.stop(true, false);
-			$projectDesc.animate(
-				{left: "-90%"}, 
-				'250', 
-				'swing',
-				function() {
-					var height = $projectDesc.heightFinder.height();
-					$projectDesc.title.html(selectedProject.title);
-					$projectDesc.description.html(selectedProject.description);
-					$projectDesc.outcomes.html(selectedProject.outcomes);
-					$projectDesc.lessons.html(selectedProject.lessons);
-					$projectDesc.nHeight = $projectDesc.heightFinder.height();
-					console.log(height + ', ' + $projectDesc.nHeight);
-					//$projectDesc.css('height', height + 'px');
-				}
-			).delay(100).animate(
-				{left: "0%"}, 
-				'250', 
-				'swing',
-				function() {
-					projectMoveable = true;
-				}
-			);*/
+	//Clicking the project pics
+	$('.project').on('click', function(){	
+		var id = $(this).attr('id');
+		if(id == "ER") {
+			selectedProject = projectsArray.ER;
+		} else if(id == "NEAT") {
+			selectedProject = projectsArray.NEAT;
+		} else if(id == "dSA") {
+			selectedProject = projectsArray.dSA;
+		} else if(id == "HH") {
+			selectedProject = projectsArray.HH;
+		} else if(id == "IGW") {
+			selectedProject = projectsArray.IGW;
+		} else if(id == "DS") {
+			selectedProject = projectsArray.DSA;
 		}
+
+		
+		
 	});
 
 }
@@ -160,6 +142,9 @@ function onResize(){
 	$projectHeadT.css('top', $projectHeadCont.height() / 2 - $projectHeadT.height() / 2 + 'px');
 	$wrapper.css('width', $(window).width() + 'px');
 
+	for (var i = 0; i < $projects.length; i++) {
+		cutOffText($projects[i],  $projects[i].text);
+	};
 }
 
 function scrollSet() {
@@ -173,4 +158,25 @@ function scroll(middle, t) {
 	if(middle) scrollY += -$(window).height() / 2 + $scrollLoc.outerHeight() / 2;
 	var body = $("body");
 	body.stop().animate({scrollTop:  scrollY}, '500', 'swing');
+}
+
+function cutOffText(par, obj) {
+    var wordArray = projectsArray["ER"].description.split(' ');
+    obj.css('overflow', 'scroll');
+    var disp = obj.css('display');
+    obj.css('display', 'block');
+    obj.height(par.height());
+    //Add read more
+    console.log(obj[0].scrollHeight + ", " + par.height());
+    obj.html(wordArray.join(' ') + ' <a href="http://www.google.com">read more</a>');
+    if (obj[0].scrollHeight > par.height()) { //If too long
+    	obj.html(wordArray.join(' ')); //Get rid of read more and shorten
+    	do {
+	        wordArray.pop();
+	        obj.html(wordArray.join(' ') + '... <a href="http://www.google.com">read more</a>');
+	    } while(obj[0].scrollHeight > par.height());
+    }
+
+    obj.css('overflow', 'hidden');
+    obj.css('display', disp);
 }
