0d4da7aad364ce7b986832b0b3d04a82346b7806
Unknown <crschriever@gmail.com>
2017-04-05 16:49:24 -0400

    More modal improvements

--- main.js
+++ main.js
@@ -24,35 +24,25 @@ var projectsArray = {
 	dSA: {
 		title: "deSchApps", 
 		description:
-			"<p>A support website for the iPhone Apps I created. The website has a CMS used for easy addition of new Apps.</p>"
+			'<p>A support website for the iPhone Apps I created. The website has a CMS used for easy addition of new Apps. I wrote the CMS using php.</p>'
 			+ '<h5 class="bold">Link to the website: </h5><div class="btn btn-default contact-button"><a class="contact-a" href="http://deschapps.com" target="_blank"></a>deSchApps</div>'
-		/*outcomes:
-			"A website with details on each of the apps I've worked on. I hope to improve the website's mobile version for I created if before I knew BootStrap.",
-		lessons:
-			"I learned HTML, CSS, JavaScript, PHP, and SQL through this project. I also learned the process for publishing a website through HostGator."*/
 	},
 	HH: {
 		title: "Homework Help", 
 		description:
-			"Homework Help combines scrap paper and a calculator into one app. It provides a means of sorting assignments through the app.",
-		/*outcomes:
-			"This was submitted to a Congressional STEM competition and won. It was also on the App Store but has since been removed for monetary reasons.",
-		lessons:
-			"I learned Objective C, how to navigate XCode, and the process of submitting an app to the app store."*/
+			'Homework Help combines scrap paper and a calculator into one app. It provides a means of sorting assignments inside the app. It was written in Objective C.'
+			+ '<h5 class="bold">Link to support page: </h5><div class="btn btn-default contact-button"><a class="contact-a" href="http://deschapps.com/app/HomeworkHelp" target="_blank"></a>Homework Help</div>'
 	},
 	IGW: {
 		title: "Intergalactic Warfare", 
 		description:
-			"Intergalactic Warfare is an iPhone game that consists of firing missiles at your opponents. These missiles must travel through an obstacle course of planets that all have a gravitational pull that affects the path of the missile",
-		/*outcomes:
-			"This app was on the app store but has since been removed for monetary reasons.",
-		lessons:
-			"Through the creation of this app I learned how to use XCode's SpriteKit library."*/
+			'Intergalactic Warfare is an iPhone game that consists of firing missiles at your opponents. These missiles must travel through an obstacle course of planets that all have a gravitational pull that affects the path of the missile. It was written in Objective C.'
+			+ '<h5 class="bold">Link to support page: </h5><div class="btn btn-default contact-button"><a class="contact-a" href="http://deschapps.com/app/IntergalacticWarfare" target="_blank"></a>Intergalactic Warfare</div>'
 	},
 	DSA: {
 		title: "Darlington School App", 
 		description:
-			"This is an app created for my upper school. It displayed to user's grades, assignments, announcements, schedules, etc.",
+			"This is an app created for my upper school. It displayed user's grades, assignments, announcements, schedules, etc. It was written with HTML, CSS, and Javascript.",
 		/*outcomes:
 			"I took an independent study at my high school and created this app using PhoneGap and HTML.",
 		lessons:
