ae31efe8f6509e2bdf5373e51ee54511f55ef0c4
crschriever <crschriever@gmail.com>
2018-08-01 20:24:39 -0400

    Move quote, fiddle with header

--- public/dist/main.bundle.js
+++ public/dist/main.bundle.js
@@ -9937,7 +9937,7 @@ module.exports = function (module) {
 /* WEBPACK VAR INJECTION */(function($) {
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+    value: true
 });
 exports.mobile = undefined;
 
@@ -9952,103 +9952,104 @@ var mobile = exports.mobile = false;
 
 //initializes variables whenever the page is loaded
 function startUp() {
-	$mobileDet = $('.mobile-detection');
-	exports.mobile = mobile = $mobileDet.width() > 0;
+    $mobileDet = $(".mobile-detection");
+    exports.mobile = mobile = $mobileDet.width() > 0;
 
-	// Set up menu and menu button for mobile
-	$menuSelect = $('.menu-button');
-	$menu = $('.mobile-nav');
-	var $menuXDiv = $menu.find('div');
-	$menuXDiv.css("height", $menuXDiv.find(".exit-button").height() + 'px');
-	$menuSelect.on('click', function (event) {
-		$menu.animate({ "left": "0px" }, 'fast');
-		$menuSelect.animate({ "opacity": "0" }, 'fast');
-	});
-	$('.wrapper').on('click', function () {
-		closeMenu();
-	});
-	$menu.find('.exit-button').on('click', function () {
-		closeMenu();
-	});
+    // Set up menu and menu button for mobile
+    $menuSelect = $(".menu-button");
+    $menu = $(".mobile-nav");
+    var $menuXDiv = $menu.find("div");
+    $menuXDiv.css("height", $menuXDiv.find(".exit-button").height() + "px");
+    $menuSelect.on("click", function (event) {
+        $menu.animate({ left: "0px" }, "fast");
+        $menuSelect.animate({ opacity: "0" }, "fast");
+    });
+    $(".wrapper").on("click", function () {
+        closeMenu();
+    });
+    $menu.find(".exit-button").on("click", function () {
+        closeMenu();
+    });
 
-	$(window).resize(onResize);
+    $(window).resize(onResize);
 
-	scrollSet();
+    scrollSet();
 
-	// Tooltip init;
-	$('[data-toggle="tooltip"]').tooltip();
+    // Tooltip init;
+    $('[data-toggle="tooltip"]').tooltip();
 }
 
 function onResize() {
-	exports.mobile = mobile = $mobileDet.width() > 0;
+    exports.mobile = mobile = $mobileDet.width() > 0;
 }
 
 // Sets up event listeners for navigation buttons
 function scrollSet() {
-	$('a[href$="#scroll"]').on('click', function (e) {
-		e.preventDefault();
-		scroll(false, this);
-	});
-	$('a[href$="#scrollmiddle"]').on('click', function (e) {
-		e.preventDefault();
-		scroll(true, this);
-	});
+    $('a[href$="#scroll"]').on("click", function (e) {
+        e.preventDefault();
+        scroll(false, this);
+    });
+    $('a[href$="#scrollmiddle"]').on("click", function (e) {
+        e.preventDefault();
+        scroll(true, this);
+    });
 }
 
 // Scrolls page
 function scroll(middle, t) {
-	var $scrollLoc = $($(t).data("scroll-to"));
-	console.log($scrollLoc);
-	var scrollY = $scrollLoc.offset().top;
-	if (middle) {
-		scrollY += -$(window).height() / 2 + $scrollLoc.outerHeight() / 2;
-	}
+    var $scrollLoc = $($(t).data("scroll-to"));
+    console.log($scrollLoc);
+    var scrollY = $scrollLoc.offset().top;
+    if (middle) {
+        scrollY += -$(window).height() / 2 + $scrollLoc.outerHeight() / 2;
+    }
 
-	$("html,body").animate({ scrollTop: scrollY }, 500, "swing");
-	// Closes when a button is pressed in menu
-	closeMenu();
+    $("html,body").animate({ scrollTop: scrollY }, 500, "swing");
+    // Closes when a button is pressed in menu
+    closeMenu();
 }
 
 function closeMenu() {
-	$menu.animate({ "left": "-81vw" }, 'fast');
-	$menuSelect.animate({ "opacity": "1" }, 'fast');
+    $menu.animate({ left: "-81vw" }, "fast");
+    $menuSelect.animate({ opacity: "1" }, "fast");
 }
 
 // Lazy loading
 function lazyLoad() {
-	// font
-	$('head').append('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">');
-	$('head').append('<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway:200,400,800">');
+    // font
+    $("head").append('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">');
+    $("head").append('<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway:200,400,800,1000">');
 
-	// images
-	var $lazyLoadImages = $('img.lazy-load').toArray();
-	$lazyLoadImages.forEach(function (image) {
-		var $image = $(image);
-		$image.attr('src', $image.data('src'));
-		$image.on('load', function () {
-			$image.removeAttr('data-src');
-			$image.css('background-color', 'rgba(0,0,0,0)');
-		});
-	});
+    // images
+    var $lazyLoadImages = $("img.lazy-load").toArray();
+    $lazyLoadImages.forEach(function (image) {
+        var $image = $(image);
+        $image.attr("src", $image.data("src"));
+        $image.on("load", function () {
+            $image.removeAttr("data-src");
+            $image.css("background-color", "rgba(0,0,0,0)");
+        });
+    });
 
-	var $lazyLoadBackgrounds = $('div.lazy-load').toArray();
-	$lazyLoadBackgrounds.forEach(function (bg) {
-		var $bg = $(bg);
-		if (mobile && $bg.hasClass('small-img')) {
-			$bg.css('background-image', 'url(' + $bg.data('src').split('.').join('-small.') + ')');
-		} else {
-			$bg.css('background-image', 'url(' + $bg.data('src') + ')');
-		}
-		$bg.on('load', function () {
-			$bg.removeAttr('data-src');
-			$bg.css('background-color', 'rgba(0,0,0,0)');
-		});
-	});
+    var $lazyLoadBackgrounds = $("div.lazy-load").toArray();
+    $lazyLoadBackgrounds.forEach(function (bg) {
+        var $bg = $(bg);
+        if (mobile && $bg.hasClass("small-img")) {
+            $bg.css("background-image", "url(" + $bg.data("src").split(".").join("-small.") + ")");
+        } else {
+            $bg.css("background-image", "url(" + $bg.data("src") + ")");
+        }
+        $bg.on("load", function () {
+            $bg.removeAttr("data-src");
+            $bg.css("background-color", "rgba(0,0,0,0)");
+        });
+    });
 }
 
 $(startUp);
 $(window).on("load", function () {
-	startUp();lazyLoad();
+    startUp();
+    lazyLoad();
 });
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
@@ -12314,7 +12315,7 @@ function onResize() {
         $tTextCont.removeAttr("style");
     } else {
         $tCont.css("padding-top", 50 + $tNavCont.height() + "px");
-        $tTextCont.css("top", $tCont.height() / 2 - $tTextCont.height() / 2 /*- $tNavCont.height()*/ + "px");
+        $tTextCont.css("top", $tCont.height() / 2 - $tTextCont.height() / 2 - $tNavCont.height() + "px");
     }
 
     $projectHeadT.css("top", $projectHeadCont.height() / 2 - $projectHeadT.height() / 2 + "px");
--- public/src/js/common.js
+++ public/src/js/common.js
@@ -1,5 +1,5 @@
-import 'jquery/dist/jquery.js';
-import 'bootstrap-sass/assets/javascripts/bootstrap.js';
+import "jquery/dist/jquery.js";
+import "bootstrap-sass/assets/javascripts/bootstrap.js";
 
 let $mobileDet;
 let $menuSelect;
@@ -8,99 +8,114 @@ export var mobile = false;
 
 //initializes variables whenever the page is loaded
 function startUp() {
-    $mobileDet = $('.mobile-detection');
-	mobile = $mobileDet.width() > 0;
+    $mobileDet = $(".mobile-detection");
+    mobile = $mobileDet.width() > 0;
 
-	// Set up menu and menu button for mobile
-	$menuSelect = $('.menu-button');
-	$menu = $('.mobile-nav');
-	let $menuXDiv = $menu.find('div');
-	$menuXDiv.css("height", $menuXDiv.find(".exit-button").height() + 'px');
-	$menuSelect.on('click', function(event) {
-		$menu.animate({"left": "0px"},'fast');
-		$menuSelect.animate({"opacity": "0"},'fast');
-	});
-	$('.wrapper').on('click', function() {
-		closeMenu();
-	});
-	$menu.find('.exit-button').on('click', function() {
-		closeMenu();
-	});
-    
-    $( window ).resize(onResize);
+    // Set up menu and menu button for mobile
+    $menuSelect = $(".menu-button");
+    $menu = $(".mobile-nav");
+    let $menuXDiv = $menu.find("div");
+    $menuXDiv.css("height", $menuXDiv.find(".exit-button").height() + "px");
+    $menuSelect.on("click", function(event) {
+        $menu.animate({ left: "0px" }, "fast");
+        $menuSelect.animate({ opacity: "0" }, "fast");
+    });
+    $(".wrapper").on("click", function() {
+        closeMenu();
+    });
+    $menu.find(".exit-button").on("click", function() {
+        closeMenu();
+    });
 
-	scrollSet();
-	
-	// Tooltip init;
-	$('[data-toggle="tooltip"]').tooltip();
+    $(window).resize(onResize);
+
+    scrollSet();
+
+    // Tooltip init;
+    $('[data-toggle="tooltip"]').tooltip();
 }
 
-function onResize() { 
+function onResize() {
     mobile = $mobileDet.width() > 0;
 }
 
 // Sets up event listeners for navigation buttons
 function scrollSet() {
-	$('a[href$="#scroll"]').on('click', function(e){
-		e.preventDefault();
-		scroll(false, this);
-	});
-	$('a[href$="#scrollmiddle"]').on('click', function(e){
-		e.preventDefault();
-		scroll(true, this);
-	});
+    $('a[href$="#scroll"]').on("click", function(e) {
+        e.preventDefault();
+        scroll(false, this);
+    });
+    $('a[href$="#scrollmiddle"]').on("click", function(e) {
+        e.preventDefault();
+        scroll(true, this);
+    });
 }
 
 // Scrolls page
 function scroll(middle, t) {
-	var $scrollLoc = $($(t).data("scroll-to"));
-	console.log($scrollLoc);
-	var scrollY = $scrollLoc.offset().top;
-	if (middle) {
-		scrollY += -$(window).height() / 2 + $scrollLoc.outerHeight() / 2;
-	}
-	
-	$("html,body").animate({ scrollTop: scrollY }, 500, "swing");
-	// Closes when a button is pressed in menu
-	closeMenu();
+    var $scrollLoc = $($(t).data("scroll-to"));
+    console.log($scrollLoc);
+    var scrollY = $scrollLoc.offset().top;
+    if (middle) {
+        scrollY += -$(window).height() / 2 + $scrollLoc.outerHeight() / 2;
+    }
+
+    $("html,body").animate({ scrollTop: scrollY }, 500, "swing");
+    // Closes when a button is pressed in menu
+    closeMenu();
 }
 
 function closeMenu() {
-	$menu.animate({"left": "-81vw"},'fast');
-	$menuSelect.animate({"opacity": "1"},'fast');
+    $menu.animate({ left: "-81vw" }, "fast");
+    $menuSelect.animate({ opacity: "1" }, "fast");
 }
 
 // Lazy loading
 function lazyLoad() {
-	// font
-	$('head').append('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">');
-    $('head').append('<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway:200,400,800">');
+    // font
+    $("head").append(
+        '<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">'
+    );
+    $("head").append(
+        '<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway:200,400,800,1000">'
+    );
 
-	// images
-	let $lazyLoadImages = $('img.lazy-load').toArray();
-	$lazyLoadImages.forEach(function(image) {
-		let $image = $(image);
-		$image.attr('src', $image.data('src'));
-		$image.on('load', function() {
-			$image.removeAttr('data-src');
-			$image.css('background-color', 'rgba(0,0,0,0)');			
-		});
-	});
+    // images
+    let $lazyLoadImages = $("img.lazy-load").toArray();
+    $lazyLoadImages.forEach(function(image) {
+        let $image = $(image);
+        $image.attr("src", $image.data("src"));
+        $image.on("load", function() {
+            $image.removeAttr("data-src");
+            $image.css("background-color", "rgba(0,0,0,0)");
+        });
+    });
 
-	let $lazyLoadBackgrounds = $('div.lazy-load').toArray();
-	$lazyLoadBackgrounds.forEach(function(bg) {
-		let $bg = $(bg);
-		if (mobile && $bg.hasClass('small-img')) {
-			$bg.css('background-image', 'url(' + $bg.data('src').split('.').join('-small.') + ')');			
-		} else {
-			$bg.css('background-image', 'url(' + $bg.data('src') + ')');
-		}
-		$bg.on('load', function() {
-			$bg.removeAttr('data-src');
-			$bg.css('background-color', 'rgba(0,0,0,0)');
-		});
-	});
+    let $lazyLoadBackgrounds = $("div.lazy-load").toArray();
+    $lazyLoadBackgrounds.forEach(function(bg) {
+        let $bg = $(bg);
+        if (mobile && $bg.hasClass("small-img")) {
+            $bg.css(
+                "background-image",
+                "url(" +
+                    $bg
+                        .data("src")
+                        .split(".")
+                        .join("-small.") +
+                    ")"
+            );
+        } else {
+            $bg.css("background-image", "url(" + $bg.data("src") + ")");
+        }
+        $bg.on("load", function() {
+            $bg.removeAttr("data-src");
+            $bg.css("background-color", "rgba(0,0,0,0)");
+        });
+    });
 }
 
 $(startUp);
-$(window).on("load", function() {startUp(); lazyLoad();});
+$(window).on("load", function() {
+    startUp();
+    lazyLoad();
+});
