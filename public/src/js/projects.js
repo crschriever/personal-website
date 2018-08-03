98bbee840fd6bda1220134de3dca78b4c244820e
crschriever <crschriever@gmail.com>
2018-08-03 19:15:41 -0400

    Rewording some projects

--- public/src/js/projects.js
+++ public/src/js/projects.js
@@ -2,8 +2,8 @@ export let projectsArray = {
     CI: {
         title: "Cicerone",
         description: `
-			<p>Cicerone is an android app that tells you about any interesting location you happen to pass by, whether you're walking, driving, or flying. The core idea is to let the user explore while the application highlights what is nearby. It will automatically detect nearby interesting locations and tell you about them using text-to-speech even if the app is in the background or your phone is closed. Go into silent mode and receive push notifications instead of voice.</p>
-			<p>Cicerone is built for Android, making use of Java, Google Maps API, and mobile text-to-speech. The app itself makes use of map data and wiki information in order to describe locations in a way the user will understand.</p>
+			<p>Created by a two man team at HackGT, Cicerone is an android app that tells you about interesting locations you pass by while exploring new locations. It will automatically detect points of interest and use text-to-speech to inform you, even when your phone is closed. Go into silent mode and receive push notifications instead of voice.</p>
+			<p>Cicerone is built for Android using Java, Google Maps API, Wikipedia, and mobile text-to-speech.</p>
 			<h5 class="bold">GitHub client repo: </h5><div class="btn btn-warning contact-button"><a class="contact-a" href="https://github.com/crschriever/Cicerone" target="_blank"></a><i class="fa fa-github"></i> GitHub</div>'
 			<h5 class="bold">Read more: </h5><div class="btn btn-default contact-button"><a class="contact-a" href="https://devpost.com/software/cicerone" target="_blank"></a> Devpost</div>
 		`
@@ -31,8 +31,8 @@ export let projectsArray = {
     GK: {
         title: "Gate Keeper",
         description:
-            "<p>Gate Keeper is a project my team made at HackGSU. We wanted to make an easy, secure way for people to grant access to buildings and alleviate having to check in at the front desk everywhere you go.</p>" +
-            "<p>This solution provides a robust admin side database system to manage access based on asymmetric elliptic curve encryption to authentic access requests. Additionally, we built an intuitive client side app to make it easy to interface with the database side through NFC to easily gain access. Also, we wanted to make sure everything was secure, so we used private keys generated on the users' devices to sign access requests, which are checked against a server side database of approved credentials. Finally, for added security, we designed the system to be able to work over LAN to allow clients to run the solution on an air-gapped system for extra security.</p>" +
+            "<p>Gate Keeper is a project made by a four man team at HackGSU. We wanted an easy, secure way for people to grant access to buildings and to alleviate the need to check in at the front desk everywhere you go.</p>" +
+            "<p>This solution provides a robust admin system to manage access, secured using asymmetric elliptic curve encryption. Additionally, we built an intuitive app for users to easily gain access to secured locations through NFC.</p>" +
             '<h5 class="bold">GitHub server repo: </h5><div class="btn btn-warning contact-button"><a class="contact-a" href="https://github.com/bentemplin/gatekeeper-server" target="_blank"></a><i class="fa fa-github"></i> GitHub</div>' +
             '<h5 class="bold">GitHub client repo: </h5><div class="btn btn-warning contact-button"><a class="contact-a" href="https://github.com/bentemplin/gatekeeper-app" target="_blank"></a><i class="fa fa-github"></i> GitHub</div>' +
             '<h5 class="bold">Read more: </h5><div class="btn btn-default contact-button"><a class="contact-a" href="https://devpost.com/software/gatekeeper-v43jwb" target="_blank"></a> Devpost</div>'
@@ -40,24 +40,25 @@ export let projectsArray = {
     ER: {
         title: "ER Schedule",
         description:
-            "<p>This is a PHP based web app. It creates work schedules for Emergency Rooms, a task that normally takes an employee a full day to do. I surveyed local Emergency Room for the required features such an app would need. App accounted for preferred days off, preferred number of shifts, number of shifts worked in a row, number of weekends worked in a row, number of holidays worked, etc.<p>" +
-            '<h5 class="bold">GitHub repo: </h5><div class="btn btn-warning contact-button"><a class="contact-a" href="https://github.com/crschriever/ER-Schedule" target="_blank"></a><i class="fa fa-github"></i> GitHub</div>' +
-            '<h5 class="bold">Live Example: </h5><div class="btn btn-default contact-button"><a class="contact-a" href="http://www.scheduleir.com" target="_blank"></a> Example</div>'
+            "<p>This is a PHP based web app that creates work schedules for Emergency Rooms, a task that normally takes an employee a full day to do.</p>" +
+            "<p>I surveyed local Emergency Room for the required features such an app would need. My solution accounted for preferred and required days off, preferred number of shifts, number of shifts worked in a row, number of weekends worked in a row, number of holidays worked, etc.<p>" +
+            '<h5 class="bold">GitHub repo: </h5><div class="btn btn-warning contact-button"><a class="contact-a" href="https://github.com/crschriever/ER-Schedule" target="_blank"></a><i class="fa fa-github"></i> GitHub</div>'
     },
     NEAT: {
         title: "NEAT",
         description:
-            "<p>NEAT stands for Neuro-Evolution of Augmenting Topologies. It is a machine learning algorithm that creates and evolves neural networks to solve a task. I made my own implementation of this algorithm in Java. It was my first taste of Machine Learning and won't be my last.</p>"
+            "<p>NEAT stands for Neuro-Evolution of Augmenting Topologies. It is a machine learning algorithm that creates and updates neural networks using genetic based evolution. I made my own implementation of this algorithm in Java.</p>"
     },
     dSA: {
         title: "deSchApps",
         description:
-            "<p>A support website for the iPhone Apps I created. The website has a CMS used for easy addition of new Apps. I created an Admin Dashboard that displayed current apps and allowed you to add new apps using php.</p>"
+            "<p>A support website made with php for the iPhone Apps I created. The website had a CMS used for easy addition of new Apps. I created an Admin Dashboard that displayed current apps and allowed you to add new apps.</p>"
     },
     HH: {
         title: "Homework Help",
         description:
-            "Homework Help combines scrap paper and a calculator into one app. It provides a means of sorting assignments inside the app. Through this project I learned Xcode and Objective C. I collaborated with a graphic design artist and won a congressional STEM competition."
+            "<p>Homework Help combines scrap paper and a calculator into one app and provides a means of sorting assignments.</p>" +
+            "<p>Through this project I learned XCode and Objective C. I collaborated with a graphic design artist and won a congressional STEM competition.</p>"
     },
     IGW: {
         title: "Intergalactic Warfare",
